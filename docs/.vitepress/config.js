0deb9dd5abd1be10ff73f6de4f7f4bfd9d4ce52e
Himanshu-AT <aristech.official@gmail.com>
2024-02-12 22:02:12 +0530

    make website, docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "GitClient Docs",
+    description: "Git Client is an Open Source UI application for git cli",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "initial github", link: "https://github.com/Himasnhu-AT/ruscode.git" }
+        ],
+        sidebar: [
+            {
+                text: "Getting Started",
+                items: [
+                    { text: "Home", link: "/" },
+                ]
+            }
+        ]
+    }
+};
